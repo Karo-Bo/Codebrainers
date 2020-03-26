6bb6078750d2bb4708dd63327029b9fa8aeca84d
bochenekk <bochenekk@gmail.com>
2020-03-26 20:35:38 +0100

    web project add responsive nav section

---
+++ Project_web_home/assets/js/script.js
@@ -0,0 +1,17 @@
+// let navigation = document.getElementById("navigation");
+// window.addEventListener("scroll", function () {
+//     if (window.pageYOffset > 50) {
+//         navigation.classList.add("active");
+//     }
+//     else {
+//         navigation.classList.remove("active");
+//     }
+// })
+
+let hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('open');
+    document.body.classList.toggle('menu-open');
+})
+
+// AOS.init();
